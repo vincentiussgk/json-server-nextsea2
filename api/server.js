aad9dcf511c963435aaf0e0be52d788b1549b546
Vincentius Samuel G <vincentius.samuelgk2@gmail.com>
2023-12-21 00:42:08 +0700

    revert: revert to original server script

--- api/server.js
+++ api/server.js
@@ -1,11 +1,7 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
-const express = require("express");
-const bodyParser = require("body-parser");
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUI = require("swagger-ui-express");
-const cors = require("cors");
 
-const server = express();
+const server = jsonServer.create();
 
 // Uncomment to allow write operations
 const fs = require("fs");
@@ -15,44 +11,12 @@ const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
 
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
+
 const middlewares = jsonServer.defaults();
 
-const port = 3001;
-
-const apiUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://next-be-samuel.vercel.app"
-    : `http://localhost:${port}`;
-
-const routeIndex = require("../routes/index");
-
-server.use(cors());
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
-
-// Defining Custom APIs
-
-Object.values(routeIndex).forEach((routeIndexItem) => {
-  server.use(routeIndexItem);
-});
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    servers: [
-      {
-        url: apiUrl,
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
-const specs = swaggerJsDoc(options);
-
-server.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-
 server.use(middlewares);
-
 // Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
@@ -61,9 +25,8 @@ server.use(
   })
 );
 server.use(router);
-
-server.listen(port, () => {
-  console.log("Server is running");
+server.listen(3000, () => {
+  console.log("JSON Server is running");
 });
 
 // Export the Server API
