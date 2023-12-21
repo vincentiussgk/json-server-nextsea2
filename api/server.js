3a960d0a331cb6b13258dfdeb6bb1ae38f95e454
Vincentius Samuel G <vincentius.samuelgk2@gmail.com>
2023-12-21 23:41:42 +0700

    feat: add cors

--- api/server.js
+++ api/server.js
@@ -10,12 +10,14 @@ const filePath = path.join("db.json");
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
+const cors = require("cors");
 
 // Comment out to allow write operations
 // const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
